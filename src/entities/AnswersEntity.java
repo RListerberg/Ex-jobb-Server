5e678c88166dac93b327d2f11e6a87e95e012f81
RListerberg <Robin_listerberg@hotmail.com>
2017-01-24 15:09:54 +0100

    Started a server git repo

---
+++ src/entities/AnswersEntity.java
@@ -0,0 +1,11 @@
+package entities;
+
+/**
+ * Created by robin on 2017-01-24.
+ */
+public class AnswersEntity {
+
+	private int answerID;
+	private String answer;
+
+}
---
+++ src/entities/CategoriesEntity.java
@@ -0,0 +1,7 @@
+package entities;
+
+/**
+ * Created by robin on 2017-01-24.
+ */
+public class CategoriesEntity {
+}
